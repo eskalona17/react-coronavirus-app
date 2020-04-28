2ca5a217a2521d1550ddae84c75bdb9f3749aebd
eskalona17 <eskalona_17@hotmail.com>
2020-04-28 16:40:45 +0200

    testing with hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,93 @@
-import React, { Component } from "react";
-import Data from "./Data";
+// import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import DataList from "./DataList";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    deaths: null,
-    confirmed: null,
-    recovered: null,
-    loading: true,
-  };
 
-  async componentDidMount() {
-    try {
-      const response = await fetch(
-        "https://enrichman.github.io/covid19/world/full.json"
-      );
-      const data = await response.json();
-      this.setState({
-        deaths: data.deaths,
-        confirmed: data.confirmed,
-        recovered: data.recovered,
-        loading: false,
-      });
-    } catch (e) {
-      console.log(e);
+  //asi se crearia SIN HOOKS
+// class App extends Component {
+
+  // state = {
+  //   deaths: null,
+  //   confirmed: null,
+  //   recovered: null,
+  //   loading: true,
+  // };
+
+//   async componentDidMount() {
+//     try {
+//       const response = await fetch(
+//         "https://enrichman.github.io/covid19/world/full.json"
+//       );
+//       const data = await response.json();
+//       this.setState({
+//         deaths: data.deaths,
+//         confirmed: data.confirmed,
+//         recovered: data.recovered,
+//         loading: false,
+//       });
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
+//   render() {
+  // if(this.state.loading) {
+  //   return <div>Cargando los datos de hoy...</div>
+  // }
+  // return(
+  //   <>
+  //     <DataList
+  //     confirmed={confirmed}
+  //     deaths={deaths}
+  //     recovered={recovered}
+  //   />
+  //   </>
+  // )
+   
+// }
+// }
+
+//CON HOOKS
+const App = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [confirmed, setConfirmed] = useState(null);
+  const [deaths, setDeaths] = useState(null);
+  const [recovered, setRecovered] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://enrichman.github.io/covid19/world/full.json"
+        );
+        const data = await response.json();
+        setLoading(false)
+        setDeaths(data.deaths)
+        setConfirmed(data.confirmed)
+        setRecovered(data.recovered)
+      } catch (e) {
+        console.log(e);
+      }
     }
+    fetchData()
+  
+  },[])
+
+
+  if (loading) {
+    return <div>Cargando los datos de hoy...</div>;
   }
-  render() {
-    if (this.state.loading) {
-      return <div>Cargando los datos de hoy...</div>;
-    }
-    return (
-      <>
-      <Data
-        confirmed={this.state.confirmed}
-        deaths={this.state.deaths}
-        recovered={this.state.recovered}
-      />
-      </>
-    );
-  }
+  return (
+    
+    <DataList
+      confirmed={confirmed}
+      deaths={deaths}
+      recovered={recovered}
+    />
+    
+  );
 }
+
 
 export default App;
